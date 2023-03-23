cebf3a2fe95d1bf9164826fcd591ddbd6271ab0b
Ilya Buzuev <ilya.buzuev@mail.ru>
2023-03-24 02:40:40 +0300

    added new songs

--- src/data/data.js
+++ src/data/data.js
@@ -3,20 +3,55 @@ const songs = {
     id: 0,
     author: "IlyaB",
     title: "Sadness",
-    cover: "./src/img/covers/sadness.jpg"
+    cover: "./src/img/covers/album-cover.jpg"
   },
   rain: {
     id: 1,
     author: "IlyaB",
     title: "Rain",
-    cover: "./src/img/covers/rain.jpg",
+    cover: "./src/img/covers/album-cover.jpg",
+  },
+  march: {
+    id: 2,
+    author: "IlyaB",
+    title: "March",
+    cover: "./src/img/covers/album-cover.jpg",
+  },
+  untilPostPunkTearsUsApart: {
+    id: 3,
+    author: "IlyaB",
+    title: "Until post-punk tears us apart",
+    cover: "./src/img/covers/album-cover.jpg",
+  },
+  dawn: {
+    id: 4,
+    author: "IlyaB",
+    title: "Dawn",
+    cover: "./src/img/covers/album-cover.jpg",
+  },
+  finalfinalQualifyingWork: {
+    id: 5,
+    author: "IlyaB",
+    title: "Final qualifying work",
+    cover: "./src/img/covers/album-cover.jpg",
+  },
+  may: {
+    id: 6,
+    author: "IlyaB",
+    title: "May",
+    cover: "./src/img/covers/album-cover.jpg",
   },
 }
 
 
 export const data = {
   songList: [
-    songs.rain,
     songs.sadness,
+    songs.rain,
+    songs.march,
+    songs.untilPostPunkTearsUsApart,
+    songs.dawn,
+    songs.finalfinalQualifyingWork,
+    songs.may
   ]
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,57 @@
 import { data } from "../data/data.js";
 
-const songs = document.querySelectorAll('.song');
-const playButton = document.querySelectorAll('.song__button-play');
-const pauseButton = document.querySelectorAll('.song__button-pause');
+let songs;
+let playButton;
+let pauseButton;
+let songsList = document.getElementById('songsList');
 
 let soundList;
 let currentPlayback = {};
+
+function formatSongTitle(song) {
+  let formatedString = '';
+
+  formatedString = `${(song.author).toLowerCase()}-${(song.title).toLowerCase()}`;
+  formatedString = formatedString.replaceAll(' ', '_');
   
-soundList = createSoundList();
+  return formatedString;
+}
+
+function renderSongs() {
+  let dataSongs;
+
+  dataSongs = data.songList;
+
+  dataSongs.forEach((song, index) => {
+    songsList.innerHTML += `
+      <li class="songs__item song" data-song-title=${formatSongTitle(song)} data-id=${index}>
+        <div class="song__left">
+          <div class="song__box">
+            <img class="song__cover" src=${song.cover} alt=${song.author, "-", song.title}>
+            <button class="song__button song__button-play button-reset">
+              <svg class="song__play-icon" width="28" height="32" viewBox="0 0 28 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M27 14.2679C28.3333 15.0377 28.3333 16.9623 27 17.7321L3 31.5885C1.66667 32.3583 0 31.396 0 29.8564V2.14359C0 0.603993 1.66667 -0.358258 3 0.411542L27 14.2679Z" fill="#333333"/>
+              </svg>                        
+            </button>
+            <button class="song__button song__button-pause button-reset display-none">
+              <svg class="song__pause-icon" width="28" height="32" viewBox="0 0 28 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="11.4074" height="32" rx="2" fill="#333333"/>
+                <rect x="16.5927" width="11.4074" height="32" rx="2" fill="#333333"/>
+              </svg>                    
+            </button>
+          </div>
+          <div class="song__name">
+            <h3 class="song__title">${song.title}</h3>
+            <h4 class="song__author">${song.author}</h4>
+          </div>
+        </div>
+        <div class="song__right">
+          <p class="song__duration">4:16</p>
+        </div>
+      </li>
+    `   
+  })
+}
 
 function createSoundList() {
   let soundList = [];
@@ -50,6 +94,14 @@ function checkSongPlayback() {
 
 function main() {
   let currentSong;
+
+  renderSongs();
+
+  songs = document.querySelectorAll('.song');
+  playButton = document.querySelectorAll('.song__button-play');
+  pauseButton = document.querySelectorAll('.song__button-pause');
+
+  soundList = createSoundList();
 
   songs.forEach(song => {
     song.addEventListener('click', () => {
